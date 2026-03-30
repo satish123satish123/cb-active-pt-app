82b4c8b5c48e8d2a2ab0a8e6113151f542f53ba4
svsatish820 <svsatish820@gmail.com>
2026-03-30 11:05:55 +0530

    quasar build

---
+++ dist/spa/assets/auth-BwjDtSPA.js
@@ -0,0 +1,1 @@
+import{t as e}from"./authStore-e2yCQJ16.js";import{t}from"./axios-CE24codV.js";function n(e){return e}var r=n(async({router:n,store:r})=>{let i=e(r);n.beforeEach(async e=>{let n=e.matched.some(e=>e.meta.requiresAuth),r=i.token_id;if(n&&!r)return`/login`;if(e.path===`/login`&&r)return`/`;r&&(t.defaults.headers.common.Authorization=`Bearer ${r}`)})});export{r as default};
