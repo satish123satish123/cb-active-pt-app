6978d6f21a4dad78ff1a187ded618be832793a20
svsatish820 <svsatish820@gmail.com>
2026-04-04 10:35:12 +0530

    quasar build

--- dist/spa/assets/auth-Clm8NlBO.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./authStore-BiXO4zsS.js";import{t}from"./axios-tcs5PhLX.js";function n(e){return e}var r=n(async({router:n,store:r})=>{let i=e(r);n.beforeEach(async e=>{let n=e.matched.some(e=>e.meta.requiresAuth),r=i.token_id;if(n&&!r)return`/login`;if(e.path===`/login`&&r)return`/`;r&&(t.defaults.headers.common.Authorization=`Bearer ${r}`)})});export{r as default};
