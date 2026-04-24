8b2b245cc252bd6fdfa258c478aa6d085c6f7bc4
svsatish820 <svsatish820@gmail.com>
2026-04-24 17:10:01 +0530

    quasar build

--- dist/spa/assets/auth-B5Km4Lrb.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./authStore-mKoyp-W7.js";import{t}from"./axios-bF26An0-.js";function n(e){return e}var r=n(async({router:n,store:r})=>{let i=e(r);n.beforeEach(async e=>{let n=e.matched.some(e=>e.meta.requiresAuth),r=i.token_id;if(n&&!r)return`/login`;if(e.path===`/login`&&r)return`/`;r&&(t.defaults.headers.common.Authorization=`Bearer ${r}`)})});export{r as default};
---
+++ dist/spa/assets/auth-CgvOEGzY.js
@@ -0,0 +1,1 @@
+import{t as e}from"./authStore-C4Me-s6I.js";import{t}from"./axios-CZo_E2Rn.js";function n(e){return e}var r=n(async({router:n,store:r})=>{let i=e(r);n.beforeEach(async e=>{let n=e.matched.some(e=>e.meta.requiresAuth),r=i.token_id;if(n&&!r)return`/login`;if(e.path===`/login`&&r)return`/`;r&&(t.defaults.headers.common.Authorization=`Bearer ${r}`)})});export{r as default};
